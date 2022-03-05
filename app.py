7ebd8bc001dacc9777aea70de2e234b8d4bfc642
K2122355D <99530189+K2122355D@users.noreply.github.com>
2022-03-05 15:28:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 from flask import Flask
 
 
-# In[2]:
+# In[4]:
 
 
 app = Flask(__name__)
 
 
-# In[3]:
+# In[5]:
 
 
 from flask import render_template, request
@@ -22,27 +22,29 @@ import joblib
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
-        purchases = request.form.get("purchases")
-        suppcard = request.form.get("suppcard")
-        purchases = float(purchases)
-        suppcard = float(suppcard)
-        print (purchases, suppcard)
-        model1 = joblib.load('CCU_01')
-        pred1 = model1.predict([[purchases, suppcard]])
-        s1 = "The score of credit card upgrade base on decision tree is "+ str(pred1)
-        model2 = joblib.load('CCU_GB')
-        pred2 = model2.predict([[purchases, suppcard]])
-        s2 = "The score of credit card upgrade base on decision tree is "+ str(pred2)
-        model3 = joblib.load('CCU_NN')
-        pred3 = model3.predict([[purchases, suppcard]])
-        s3 = "The score of credit card upgrade base on decision tree is "+ str(pred3)
-        model4 = joblib.load('CCU_Reg')
-        pred4 = model4.predict([[purchases, suppcard]])
+        income = request.form.get("income")
+        age = request.form.get("age")
+        loan = request.form.get("loan")
+        income = float(income)
+        age = float(age)
+        loan = float(loan)
+        print (income, age, loan)
+        model1 = joblib.load('CCD_Reg')
+        pred1 = model1.predict([[income,age,loan]])
+        s1 = "The score of credit card risk default is base on regression is "+ str(pred1)
+        model2 = joblib.load('CCD_GB')
+        pred2 = model2.predict([[income,age,loan]])
+        s2 = "The score of credit card upgrade base on gradient booster is "+ str(pred2)
+        model3 = joblib.load('CCD_NN')
+        pred3 = model3.predict([[income,age,loan]])
+        s3 = "The score of credit card upgrade base on neural network is "+ str(pred3)
+        model4 = joblib.load('CCD_DT')
+        pred4 = model4.predict([[income,age,loan]])
         s4 = "The score of credit card upgrade base on decision tree is "+ str(pred4)
-        model5 = joblib.load('CCU_RF')
-        pred5 = model5.predict([[purchases, suppcard]])
-        s5 = "The score of credit card upgrade base on decision tree is "+ str(pred5)
-        return(render_template("index.html", result1 =s1, result2 ="1", result3 ="1", result4="1", result5 ="1"))
+        model5 = joblib.load('CCD_RF')
+        pred5 = model5.predict([[income,age,loan]])
+        s5 = "The score of credit card upgrade base on random forest is "+ str(pred5)
+        return(render_template("index.html", result1 =s1, result2 =s2, result3 =s3, result4=s4, result5 =s5))
     else:
         return(render_template("index.html", result1 ="2", result2 ="2", result3 ="2", result4="2", result5 ="2"))
 
